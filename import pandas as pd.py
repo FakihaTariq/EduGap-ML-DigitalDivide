d5bac092238afccd9204149b41a8b2517716b835
FakihaTariq <154458844+FakihaTariq@users.noreply.github.com>
2026-02-25 17:48:09 -0600

    Updating/cleaning

--- import pandas as pd.py
+++ import pandas as pd.py
@@ -1,5 +1,3 @@
-import warnings
-warnings.filterwarnings('ignore')
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -8,7 +6,6 @@ from sklearn.multioutput import MultiOutputClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, accuracy_score
 from statsmodels.stats.outliers_influence import variance_inflation_factor
-
 # Accessing Dataset
 data = pd.read_csv("digital_literacy_dataset.csv")
 
@@ -18,17 +15,23 @@ data = pd.read_csv("digital_literacy_dataset.csv")
 # Found out that everywhere it says "None" it's putting it as a null value so filling it
 data.Education_Level.fillna("No School", inplace=True)
 # Check again
-print(data.isna().sum()) #0 null values everywhere
+# print(data.isna().sum()) #0 null values everywhere
 
 
-# Take a look at the data
-print(data['Basic_Computer_Knowledge_Score'].describe())  
-print(data['Internet_Usage_Score'].describe())  
-print(data['Mobile_Literacy_Score'].describe())  
+# Describe to find Average Marker
+print(data['Basic_Computer_Knowledge_Score'].describe())  # Average: 25
+print(data['Internet_Usage_Score'].describe())  # Average: 25
+print(data['Mobile_Literacy_Score'].describe())  # Average: 26
+
+print()  # Average: 25
+print(np.median(data['Internet_Usage_Score']))  # Average: 25
+print(np.median(data['Mobile_Literacy_Score']))  # Average: 26
+
 
 # Cleaning and Mapping
 def clean(column):
     data[column] = data[column].astype(str).str.strip().str.title()
+
 
 clean("Education_Level")
 clean("Household_Income")
@@ -39,8 +42,10 @@ bins = [0, 24, 40, 60, np.inf]
 labels = ['Youth', 'Early Career', 'Midlife', 'Senior']
 data['Age_Group'] = pd.cut(data['Age'], bins=bins, labels=labels)
 
+
 def map(column, mapping):
     data[column] = data[column].map(mapping)
+
 
 education_map = {'No School': 0, 'Primary': 1, 'Secondary': 2, 'High School': 3}
 income_map = {'Low': 0, 'Medium': 1, 'High': 2}
@@ -52,28 +57,33 @@ map("Household_Income", income_map)
 map("Employment_Status", employment_map)
 map("Age_Group", age_map)
 
+
 # Categorize and Labels
 def categorize_1(val):  # For computer skills and internet usage
-    if val < np.median(data['Basic_Computer_Knowledge_Score']):  # the median value
+    if val <= 25:  # the median value
         return 0  # Below Average
     else:
         return 1 # Above Average
 
+
 def categorize_2(val):  # For mobile skills
-    if val < np.median(data['Internet_Usage_Score']):
+    if val <= 25:
         return 0  # Below Average
     else:
         return 1  # Above Average
 
+
 def categorize_3(val):  # For mobile skills
-    if val < np.median(data['Mobile_Literacy_Score']):
+    if val <= 26:
         return 0  # Below Average
     else:
         return 1  # Above Average
+
 
 data['Computer_Skill_Label'] = data['Basic_Computer_Knowledge_Score'].apply(categorize_1)
 data['Internet_Usage_Label'] = data['Internet_Usage_Score'].apply(categorize_2)
 data['Mobile_Literacy_Label'] = data['Mobile_Literacy_Score'].apply(categorize_3)
+
 
 x = data[["Education_Level", "Household_Income", "Employment_Status", "Age_Group"]]
 y = data[['Computer_Skill_Label', "Internet_Usage_Label", "Mobile_Literacy_Label"]]
@@ -86,7 +96,9 @@ vif_data["VIF"] = [variance_inflation_factor(x_df.values, i) for i in range(x_df
 
 print(vif_data)
 
+
 # Splitting, Training, Model
+
 xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size=0.2, random_state=42)
 
 base_classifier = RandomForestClassifier(
@@ -101,15 +113,25 @@ base_classifier = RandomForestClassifier(
     n_jobs=-1
 )
 
+
 model = MultiOutputClassifier(base_classifier)
 
 model.fit(xtrain, ytrain)
 ypred = model.predict(xtest)
 
+
 # Convert predictions to DataFrame for easier inspection
+
 ypred_df = pd.DataFrame(ypred, columns=y.columns)
 
-# Feature Importance
+
+# Loop through each target and print classification metrics
+for label in y.columns:
+    print(f"\n=== Classification Metrics for: {label} ===")
+    print("\nClassification Report:")
+    print(classification_report(ytest[label], ypred_df[label]))
+    print("Accuracy: ", accuracy_score(ytest[label], ypred_df[label]))
+
 for i, label in enumerate(y.columns):
     print(f"\n Feature importances for: {label}")
     estimator = model.estimators_[i]
@@ -118,14 +140,15 @@ for i, label in enumerate(y.columns):
     for feature, score in zip(x.columns, importances):
         print(f"{feature:<25} {score:.4f}")
 
+# Feature Importance
 features = list(x)
-plt.figure(figsize=(8, 6))
-plt.barh(features, importances, color='skyblue')
-plt.xlabel('Importance')
-plt.title('Feature Importance')
-plt.yticks(rotation=35, size = 7)
-plt.gca().invert_yaxis()
-plt.show()
+# plt.figure(figsize=(8, 6))
+# plt.barh(features, importances, color='skyblue')
+# plt.xlabel('Importance')
+# plt.title('Feature Importance')
+# plt.yticks(rotation=35, size = 7)
+# plt.gca().invert_yaxis()
+# plt.show()
 
 # MODEL 2
 # Creating columns
@@ -134,6 +157,9 @@ data['Internet_Gain'] = data['Post_Training_Internet_Usage_Score'] - data['Inter
 data['Mobile_Gain'] = data['Post_Training_Mobile_Literacy_Score'] - data['Mobile_Literacy_Score']
 
 print(data[['Computer_Gain', 'Internet_Gain', 'Mobile_Gain']].describe())
+print(np.median(data['Computer_Gain']))
+print(np.median(data['Internet_Gain']))
+print(np.median(data['Mobile_Gain']))
 
 # Cleaning and Mapping
 clean("Engagement_Level")
@@ -142,24 +168,28 @@ engagement_map = {'Low': 0, 'Medium': 1, 'High': 2}
 
 map("Engagement_Level", engagement_map)
 
+
 # Categorize and Labels
 def categorize_1(val):  # For computer skills and internet usage
-    if val < np.median(data['Computer_Gain']):
+    if val < 36:
         return 0  # Below Average
     else:
         return 1
 
+
 def categorize_2(val):  # For mobile skills
-    if val < np.median(data['Internet_Gain']):
+    if val < 35:
         return 0  # Below Average
     else:
         return 1  # Above Average
 
+
 def categorize_3(val):  # For mobile skills
-    if val < np.median(data['Mobile_Gain']):
+    if val < 35:
         return 0  # Below Average
     else:
         return 1  # Above Average
+
 
 data['Computer_Gain_Label'] = data['Computer_Gain'].apply(categorize_1)
 data['Internet_Gain_Label'] = data['Internet_Gain'].apply(categorize_2)
@@ -198,9 +228,11 @@ model.fit(xtrain, ytrain)
 ypred = model.predict(xtest)
 
 # Convert predictions to DataFrame for easier inspection
-ypred_gain_df = pd.DataFrame(ypred, columns=y.columns)
 
-#Feature Importance
+ypred_df = pd.DataFrame(ypred, columns=y.columns)
+
+
+
 features = []
 for i, label in enumerate(y.columns):
     print(f"\n Feature importances for: {label}")
@@ -211,24 +243,32 @@ for i, label in enumerate(y.columns):
         print(f"{feature:<25} {score:.4f}")
         features.append(feature)
 
-features = list(x)
-plt.figure(figsize=(8, 6))
-plt.barh(features, importances, color='skyblue')
-plt.xlabel('Importance')
-plt.title('Feature Importance')
-plt.yticks(rotation=35, size = 7)
-plt.gca().invert_yaxis()
-plt.show()
+# Feature Importance
+# features = list(x)
+# plt.figure(figsize=(8, 6))
+# plt.barh(features, importances, color='skyblue')
+# plt.xlabel('Importance')
+# plt.title('Feature Importance')
+# plt.yticks(rotation=35, size = 7)
+# plt.gca().invert_yaxis()
+# plt.show()
 
 
-#Plotting and Calculating
-#Based on data for comparison of ML's revelations
+# Plotting and Calculating
+def predicted_inequities(df, ypred_df, group_cols, label_col, pred_cols):
+    data = xtest.reset_index(drop = True).copy()
+    data[pred_cols] = ypred_df[pred_cols].values
+    inequity_summary = (
+        data.groupby(group_cols)[pred_cols].mean().round(2).reset_index()
+    )
+    print(inequity_summary)
+
 def averages_calculate(df, group_col, label_col, title_prefix=""):
     grouped = df.groupby(group_col)[label_col].value_counts(normalize=True).unstack().fillna(0)
     grouped.columns = ['Below Average', 'Above Average']
     print(grouped)
 
-#To show ML revelations for underserved group
+
 def bar_graph(df, group_cols, label_col, top_n, title_prefix):
     worst_groups = []
     proportions = []
@@ -241,10 +281,18 @@ def bar_graph(df, group_cols, label_col, top_n, title_prefix):
             proportions.append(val)
     plt.ylabel("Proportion Below Average")
     bars = plt.bar(worst_groups, proportions, color='firebrick')
-    plt.title(f"{title_prefix}")
+    plt.title(f"{title_prefix} Most Below Average Groups")
     plt.xticks(rotation=35)
     plt.tight_layout()
     plt.show()
+
+
+# Growth in Groups (High Gain, Low Start)
+# def high_growth(df, group, skill_label_col, gain_label_col):
+#   data['Inequity_Marker'] = ((df[skill_label_col] == 0) & (df[gain_label_col] == 1)).astype(int)
+#   inequity_potential = df.groupby(group)['Inequity_Marker'].mean()
+#   print(inequity_potential)
+
 
 # Reverse maps (reapplied for labels)
 education_reverse_map = {0: 'No School', 1: 'Primary', 2: 'Secondary', 3: 'High School'}
@@ -260,98 +308,78 @@ data['Employment_Label'] = data['Employment_Status'].map(employment_reverse_map)
 data['Age_Label'] = data['Age_Group'].map(age_reverse_map)
 data['Engagement_Label'] = data['Engagement_Level']
 
-#Get Machine Learning predictions to use for bar graphs to portray models' revelations
-xtest_reset = xtest.reset_index(drop=True)
-labels_reset = data[['Education_Label', 'Income_Label', 'Employment_Label', 'Age_Label']].iloc[xtest.index].reset_index(drop=True)
-
-xtest_with_labels = pd.concat([xtest_reset, labels_reset], axis=1)
-
-group_data = xtest_with_labels[['Education_Label', 'Income_Label', 'Employment_Label', 'Age_Label']].reset_index(drop=True)
-
-pred_cols = ['Computer_Skill_Pred', 'Internet_Usage_Pred', 'Mobile_Literacy_Pred']
-pred_df = ypred_df.copy()
-pred_df.columns = pred_cols
-
-ml_data = group_data.copy()
-ml_data[pred_cols] = pred_df[pred_cols]
-
-pred_cols_gain = ['Computer_Gain_Pred', 'Internet_Gain_Pred', 'Mobile_Gain_Pred']
-pred_df_gain = ypred_gain_df.copy()
-pred_df_gain.columns = pred_cols_gain
-
-ml_data_gain = group_data.copy()
-ml_data_gain[pred_cols_gain] = pred_df_gain[pred_cols_gain]
-
+predicted_inequities(xtest, ypred_df, ['Income_Label', 'Education_Label', 'Employment_Label', 'Age_Label'], ['Computer_Skill_Label', 'Internet_Usage_Label', 'Mobile_Literacy_Label'], ['Computer_Skill_Label', 'Internet_Usage_Label', 'Mobile_Literacy_Label'])
 # Calling Computer Skill Inequity
-print("")
-print("Computer Skill Inequity")
-print("")
-averages_calculate(data, 'Income_Label', 'Computer_Skill_Label', 'ML Predicted Skills')
-averages_calculate(data, 'Education_Label', 'Computer_Skill_Label', 'Computer Skill Levels')
-averages_calculate(data, 'Employment_Label', 'Computer_Skill_Label', 'Basic_Computer_Knowledge_Score')
-averages_calculate(data, 'Age_Label', 'Computer_Skill_Label', 'Basic_Computer_Knowledge_Score')
-print("")
+#print("")
+#print("Computer Skill Inequity")
+#print("")
+#averages_calculate(data, 'Income_Label', 'Computer_Skill_Label', 'Basic_Computer_Knowledge_Score')
+#averages_calculate(data, 'Education_Label', 'Computer_Skill_Label', 'Computer Skill Levels')
+#averages_calculate(data, 'Employment_Label', 'Computer_Skill_Label', 'Basic_Computer_Knowledge_Score')
+#averages_calculate(data, 'Age_Label', 'Computer_Skill_Label', 'Basic_Computer_Knowledge_Score')
+#print("")
 
-print("")
+#print("")
 # Calling Internet Usage Inequity
-print("Internet Usage Inequity")
-print("")
-averages_calculate(data, 'Income_Label', 'Internet_Usage_Label', 'Internet_Usage_Score')
-averages_calculate(data, 'Education_Label', 'Internet_Usage_Label', 'Internet_Usage_Score')
-averages_calculate(data, 'Employment_Label', 'Internet_Usage_Label', 'Internet_Usage_Score')
-averages_calculate(data, 'Age_Label', 'Internet_Usage_Label', 'Internet_Usage_Score')
-print("")
+#print("Internet Usage Inequity")
+#print("")
+#averages_calculate(data, 'Income_Label', 'Internet_Usage_Label', 'Internet_Usage_Score')
+#averages_calculate(data, 'Education_Label', 'Internet_Usage_Label', 'Internet_Usage_Score')
+#averages_calculate(data, 'Employment_Label', 'Internet_Usage_Label', 'Internet_Usage_Score')
+#averages_calculate(data, 'Age_Label', 'Internet_Usage_Label', 'Internet_Usage_Score')
+#print("")
 
-print("")
+#print("")
 # Calling Mobile Literacy Inequity
-print("")
-print("Mobile Literacy Inequity")
-print("")
-averages_calculate(data, 'Income_Label', 'Mobile_Literacy_Label', 'Mobile_Literacy_Score')
-averages_calculate(data, 'Education_Label', 'Mobile_Literacy_Label', 'Mobile_Literacy_Score')
-averages_calculate(data, 'Employment_Label', 'Mobile_Literacy_Label', 'Mobile_Literacy_Score')
-averages_calculate(data, 'Age_Label', 'Mobile_Literacy_Label', 'Mobile_Literacy_Score')
-print("")
+#print("")
+#print("Mobile Literacy Inequity")
+#print("")
+#averages_calculate(data, 'Income_Label', 'Mobile_Literacy_Label', 'Mobile_Literacy_Score')
+#averages_calculate(data, 'Education_Label', 'Mobile_Literacy_Label', 'Mobile_Literacy_Score')
+#averages_calculate(data, 'Employment_Label', 'Mobile_Literacy_Label', 'Mobile_Literacy_Score')
+#averages_calculate(data, 'Age_Label', 'Mobile_Literacy_Label', 'Mobile_Literacy_Score')
+#print("")
 
-print("")
+#print("")
 # Calling Computer Skill Gain Inequity
-print("Computer Skill Gain")
-print("")
-averages_calculate(data, 'Income_Label', 'Computer_Gain_Label', 'Computer_Gain_Label')
-averages_calculate(data, 'Education_Label', 'Computer_Gain_Label', 'Computer_Gain_Label')
-averages_calculate(data, 'Employment_Label', 'Computer_Gain_Label', 'Computer_Gain_Label')
-averages_calculate(data, 'Age_Label', 'Computer_Gain_Label', 'Computer_Gain_Label')
-print("")
+#print("Computer Skill Gain")
+#print("")
+#averages_calculate(data, 'Income_Label', 'Computer_Gain_Label', 'Computer_Gain_Label')
+#averages_calculate(data, 'Education_Label', 'Computer_Gain_Label', 'Computer_Gain_Label')
+#averages_calculate(data, 'Employment_Label', 'Computer_Gain_Label', 'Computer_Gain_Label')
+#averages_calculate(data, 'Age_Label', 'Computer_Gain_Label', 'Computer_Gain_Label')
+#print("")
 #
-print("")
+#print("")
 # Calling Internet Usage Gain Inequity
-print("Internet Usage Gain")
-print("")
-averages_calculate(data, 'Income_Label', 'Internet_Gain_Label', 'Internet_Gain_Label')
-averages_calculate(data, 'Education_Label', 'Internet_Gain_Label', 'Internet_Gain_Label')
-averages_calculate(data, 'Employment_Label', 'Internet_Gain_Label', 'Internet_Gain_Label')
-averages_calculate(data, 'Age_Label', 'Internet_Gain_Label', 'Internet_Gain_Label')
-print("")
+#print("Internet Usage Gain")
+#print("")
+#averages_calculate(data, 'Income_Label', 'Internet_Gain_Label', 'Internet_Gain_Label')
+#averages_calculate(data, 'Education_Label', 'Internet_Gain_Label', 'Internet_Gain_Label')
+#averages_calculate(data, 'Employment_Label', 'Internet_Gain_Label', 'Internet_Gain_Label')
+#averages_calculate(data, 'Age_Label', 'Internet_Gain_Label', 'Internet_Gain_Label')
+#print("")
 #
-print("")
+#print("")
 # Calling Mobile Literacy Gain Inequity
-print("Mobile Literacy Gain")
-print("")
-averages_calculate(data, 'Income_Label', 'Mobile_Gain_Label', 'Mobile_Gain_Label')
-averages_calculate(data, 'Education_Label', 'Mobile_Gain_Label', 'Mobile_Gain_Label')
-averages_calculate(data, 'Employment_Label', 'Mobile_Gain_Label', 'Mobile_Gain_Label')
-averages_calculate(data, 'Age_Label', 'Mobile_Gain_Label', 'Mobile_Gain_Label')
-print("")
-# Graphs to see ML predictions
+#print("Mobile Literacy Gain")
+#print("")
+#averages_calculate(data, 'Income_Label', 'Mobile_Gain_Label', 'Mobile_Gain_Label')
+#averages_calculate(data, 'Education_Label', 'Mobile_Gain_Label', 'Mobile_Gain_Label')
+#averages_calculate(data, 'Employment_Label', 'Mobile_Gain_Label', 'Mobile_Gain_Label')
+#averages_calculate(data, 'Age_Label', 'Mobile_Gain_Label', 'Mobile_Gain_Label')
+#print("")
+# Graphs
 features_1 = ["Education_Label", "Income_Label", "Employment_Label", "Age_Label"]
 
-bar_graph(ml_data, features_1, 'Computer_Skill_Pred', 2, 'ML Predictions of Underserved Groups (Basic Computer Knowledge)')
-bar_graph(ml_data, features_1, 'Internet_Usage_Pred', 2, 'ML Predictions of Underserved Groups (Internet Usage)')
-bar_graph(ml_data, features_1, 'Mobile_Literacy_Pred', 2, 'ML Predictions of Underserved Groups (Mobile Literacy)')
 
-bar_graph(ml_data_gain, features_1, 'Computer_Gain_Pred', 2, 'ML Predictions of Underserved Groups (Basic Computer Knowledge Gain)')
-bar_graph(ml_data_gain, features_1, 'Internet_Gain_Pred', 2, 'ML Predictions of Underserved Groups (Internet Usage Gain)')
-bar_graph(ml_data_gain, features_1, 'Mobile_Gain_Pred', 2, 'ML Predictions of Underserved Groups (Mobile Literacy Gain)')
+# bar_graph(data, features_1, 'Computer_Skill_Label', 2, 'Basic Computer Knowledge')
+# bar_graph(data, features_1, 'Internet_Usage_Label', 2, 'Internet Usage')
+# bar_graph(data, features_1, 'Mobile_Literacy_Label', 2, 'Mobile Literacy')
+
+# bar_graph(data, features_1, 'Computer_Gain_Label', 2, 'Basic Computer Knowledge Gain')
+# bar_graph(data, features_1, 'Internet_Gain_Label', 2, 'Internet Usage Gain')
+# bar_graph(data, features_1, 'Mobile_Gain_Label', 2, 'Mobile Literacy Gain')
 
 #Graphing average values of each group for each of the label
 def adaptability(group):
@@ -393,17 +421,17 @@ def literacy(group):
     plt.show()
 
 
-adaptability("Education_Label")
-adaptability("Income_Label")
-adaptability("Employment_Label")
-adaptability("Age_Label")
+# adaptability("Education_Label")
+# adaptability("Income_Label")
+# adaptability("Employment_Label")
+# adaptability("Age_Label")
 
-application("Education_Label")
-application("Income_Label")
-application("Employment_Label")
-application("Age_Label")
+# application("Education_Label")
+# application("Income_Label")
+# application("Employment_Label")
+#application("Age_Label")
 
-literacy("Education_Label")
-literacy("Income_Label")
-literacy("Employment_Label")
-literacy("Age_Label")
+# literacy("Education_Label")
+# literacy("Income_Label")
+# literacy("Employment_Label")
+# literacy("Age_Label")
